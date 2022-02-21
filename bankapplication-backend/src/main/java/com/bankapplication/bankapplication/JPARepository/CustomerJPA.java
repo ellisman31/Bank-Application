3c84b6a548ef53ad77407b4853b89ebbf9c13b44
ellisman31 <akos99@protonmail.com>
2022-02-21 21:40:59 +0100

    feat(#6-3): JPA repository for Customer

---
+++ bankapplication-backend/src/main/java/com/bankapplication/bankapplication/JPARepository/CustomerJPA.java
@@ -0,0 +1,9 @@
+package com.bankapplication.bankapplication.JPARepository;
+
+import com.bankapplication.bankapplication.Model.Customer;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface CustomerJPA extends JpaRepository<Customer, Long> {
+}
