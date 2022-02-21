f996a506f964f8bd28d032ec1af42950cc55b1fe
ellisman31 <akos99@protonmail.com>
2022-02-21 21:50:32 +0100

    feat(#6-2): Create Transaction Service to handle transactions and set history to the Customer

---
+++ bankapplication-backend/src/main/java/com/bankapplication/bankapplication/Service/TransactionService.java
@@ -0,0 +1,57 @@
+package com.bankapplication.bankapplication.Service;
+
+import com.bankapplication.bankapplication.JPARepository.TransactionJPA;
+import com.bankapplication.bankapplication.Model.Customer;
+import com.bankapplication.bankapplication.Model.Transaction;
+import com.bankapplication.bankapplication.Types.TransactionTypes;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.math.BigDecimal;
+
+@Service
+public class TransactionService {
+
+    private final TransactionJPA transactionJPA;
+
+    @Autowired
+    public TransactionService(TransactionJPA transactionJPA) {
+        this.transactionJPA = transactionJPA;
+    }
+
+    public void saveTransaction(Transaction transaction, Boolean successFulTransaction) {
+        if (successFulTransaction) {
+            transactionJPA.save(transaction);
+        }
+    }
+
+    public void deleteTransaction(Long transactionId) {
+        transactionJPA.deleteById(transactionId);
+    }
+
+    public void setTransactionToTheCustomer(Transaction transaction) {
+
+        Customer customer = transaction.getCustomer();
+        BigDecimal customerMoney = customer.getBalance();
+        BigDecimal transactionMoney = transaction.getMoney();
+        TransactionTypes transactionType = transaction.getTransActionType();
+        Boolean successFulTransaction = false;
+
+        if (transactionType.equals(TransactionTypes.DEPOSIT)) {
+            customer.setBalance(customerMoney.add(transactionMoney));
+            successFulTransaction = true;
+        } else if (transactionType.equals(TransactionTypes.WITHDRAW)) {
+            if (customerMoney.subtract(transactionMoney).compareTo(BigDecimal.ZERO) >= 0) {
+                customer.setBalance(customerMoney.subtract(transactionMoney));
+                successFulTransaction = true;
+            }
+            else {
+                successFulTransaction = false;
+            }
+        }
+
+        saveTransaction(transaction, successFulTransaction);
+    }
+
+
+}
