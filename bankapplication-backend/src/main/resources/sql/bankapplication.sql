793d4e95c13054a64ed0848c5c30b919ccd436cd
ellisman31 <akos99@protonmail.com>
2022-02-22 01:15:12 +0100

    refactor(#4-2): rewrite SQL and add new table with new foreign keys

--- bankapplication-backend/src/main/resources/sql/bankapplication.sql
+++ bankapplication-backend/src/main/resources/sql/bankapplication.sql
@@ -8,23 +8,42 @@ CREATE TABLE public.customer
     password varchar(300),
     balance decimal,
     registration_date timestamp NOT NULL
-
 );
 
+ALTER TABLE IF EXISTS ONLY public.transfer DROP CONSTRAINT IF EXISTS fk_customer_id CASCADE;
+
+DROP TABLE IF EXISTS public.transfer CASCADE;
+CREATE TABLE public.transfer
+(
+    id          serial  NOT NULL PRIMARY KEY,
+    customer_id integer,
+    transfer_sender_name varChar(200),
+    transfer_money decimal
+);
+
+ALTER TABLE ONLY public.transfer
+    ADD CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES public.customer(id);
+
 ALTER TABLE IF EXISTS ONLY public.transaction DROP CONSTRAINT IF EXISTS fk_customer_id CASCADE;
+ALTER TABLE IF EXISTS ONLY public.transaction DROP CONSTRAINT IF EXISTS fk_transfer_id CASCADE;
 
 DROP TABLE IF EXISTS public.transaction CASCADE;
 CREATE TABLE public.transaction
 (
     id          serial  NOT NULL PRIMARY KEY,
     customer_id integer,
+    transfer_id integer,
     transaction_type varChar(150),
     money decimal
 );
 
-
+ALTER TABLE ONLY public.transaction
+    ADD CONSTRAINT fk_transfer_id FOREIGN KEY (transfer_id) REFERENCES public.transfer(id);
 ALTER TABLE ONLY public.transaction
     ADD CONSTRAINT fk_customer_id FOREIGN KEY (customer_id) REFERENCES public.customer(id);
+
+
+
 
 INSERT INTO public.customer VALUES (1, 'Oakley', 'Burns', 'burns.oakley@gmail.com', '1234', 3000, CURRENT_DATE);
 INSERT INTO public.customer VALUES (2, 'Chase', 'Price', 'price.chase@gmail.com', '1234', 3000, CURRENT_DATE);
