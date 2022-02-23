8716641a680f982659ca565fbd3b422a0d87dc08
ellisman31 <akos99@protonmail.com>
2022-02-23 02:51:31 +0100

    feat(#6-3): add repository annotation to JPA repositories

--- bankapplication-backend/src/main/java/com/bankapplication/bankapplication/JPARepository/TransactionJPA.java
+++ bankapplication-backend/src/main/java/com/bankapplication/bankapplication/JPARepository/TransactionJPA.java
@@ -2,6 +2,8 @@ package com.bankapplication.bankapplication.JPARepository;
 
 import com.bankapplication.bankapplication.Model.Transaction;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface TransactionJPA extends JpaRepository<Transaction, Long> {
 }
