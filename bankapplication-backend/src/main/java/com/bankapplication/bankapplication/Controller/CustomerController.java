c52c5751e0fe59dab10bf62f1c4d03bd5feb2c2d
ellisman31 <akos99@protonmail.com>
2022-02-23 02:48:56 +0100

    feat(#10-3): request own information and balance after authentication and save roles

--- bankapplication-backend/src/main/java/com/bankapplication/bankapplication/Controller/CustomerController.java
+++ bankapplication-backend/src/main/java/com/bankapplication/bankapplication/Controller/CustomerController.java
@@ -1,8 +1,11 @@
 package com.bankapplication.bankapplication.Controller;
 
 import com.bankapplication.bankapplication.Model.Customer;
+import com.bankapplication.bankapplication.Model.Role;
+import com.bankapplication.bankapplication.Model.RoleToCustomer;
 import com.bankapplication.bankapplication.Service.CustomerService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.web.bind.annotation.*;
 
 import java.math.BigDecimal;
@@ -29,6 +32,27 @@ public class CustomerController {
         return customerService.getCustomerById(customerId);
     }
 
+    @RequestMapping(value="/api/ownInformation", method=RequestMethod.GET)
+    public Customer getOwnInformation() {
+        if (SecurityContextHolder.getContext().getAuthentication().isAuthenticated()) {
+            String email = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            return customerService.ownInformation(email);
+        } else {
+            return null;
+        }
+
+    }
+
+    @RequestMapping(value="/api/ownBalance", method=RequestMethod.GET)
+    public BigDecimal getOwnBalance() {
+        if (SecurityContextHolder.getContext().getAuthentication().isAuthenticated()) {
+            String email = (String) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+            return customerService.ownBalance(email);
+        } else {
+            return null;
+        }
+    }
+
     @RequestMapping(value="/api/deleteCustomer/{customerId}", method=RequestMethod.DELETE)
     public void deleteCustomer(@PathVariable Long customerId) {
         customerService.deleteCustomer(customerId);
@@ -39,14 +63,24 @@ public class CustomerController {
         customerService.updateCustomer(customer);
     }
 
-    @RequestMapping(value="/api/saveCustomer", method=RequestMethod.POST)
-    public void saveCustomer(@RequestBody Customer customer) {
+    @RequestMapping(value="/api/registration", method=RequestMethod.POST)
+    public void registerCustomer(@RequestBody Customer customer) {
         customerService.saveCustomer(customer);
     }
 
     @RequestMapping(value="/api/customerBalance/{customerId}", method=RequestMethod.GET)
     public BigDecimal customerBalance(@PathVariable Long customerId) {
         return customerService.customerBalance(customerId);
+    }
+
+    @RequestMapping(value="/api/customer/role", method=RequestMethod.POST)
+    public void saveCustomerRole(@RequestBody Role role) {
+        customerService.saveRole(role);
+    }
+
+    @RequestMapping(value="/api/addNewRoleToUser", method=RequestMethod.POST)
+    public void addRoleToCustomer(@RequestBody RoleToCustomer roleToCustomer) {
+        customerService.addRuleToUser(roleToCustomer.getCustomerId(), roleToCustomer.getRole());
     }
 
 }
