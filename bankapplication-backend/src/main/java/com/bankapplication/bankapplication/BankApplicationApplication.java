da3a44e7f99e36bb5eeaba55b2b85f4a7fb6ad3c
ellisman31 <akos99@protonmail.com>
2022-02-23 02:42:30 +0100

    feat(#10): create dummy users for testing all the available functions in the application

---
+++ bankapplication-backend/src/main/java/com/bankapplication/bankapplication/BankApplicationApplication.java
@@ -0,0 +1,46 @@
+package com.bankapplication.bankapplication;
+
+import com.bankapplication.bankapplication.Model.Customer;
+import com.bankapplication.bankapplication.Model.Role;
+import com.bankapplication.bankapplication.Service.CustomerService;
+import com.bankapplication.bankapplication.Types.RoleType;
+import com.bankapplication.bankapplication.Util.CustomerUtil;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.math.BigDecimal;
+
+@SpringBootApplication
+public class BankApplicationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BankApplicationApplication.class, args);
+	}
+
+	@Bean
+	PasswordEncoder passwordEncoder() {
+		return new BCryptPasswordEncoder();
+	}
+
+	@Bean
+	CommandLineRunner run(CustomerService customerService) {
+		return args -> {
+
+			customerService.saveRole(new Role(1L, RoleType.USER));
+			customerService.saveRole(new Role(2L, RoleType.ADMIN));
+
+			CustomerUtil customerUtil = new CustomerUtil();
+			customerService.saveCustomer(new Customer(1L, "Oakley", "Burns", "oakleyburns@gmail.com",
+					"1234", BigDecimal.ZERO, customerUtil.registrationDate()));
+			customerService.saveCustomer(new Customer(2L, "Chris", "May", "chrismay@gmail.com",
+					"1234", BigDecimal.ZERO, customerUtil.registrationDate()));
+
+		};
+	}
+
+
+}
