993888be470e941ebe9c066bdced2d2b8bd6ccc1
ellisman31 <akos99@protonmail.com>
2022-02-23 02:53:18 +0100

    feat(#10-4): check if the user is exists with UserDetails and get all the own information after authentication

--- bankapplication-backend/src/main/java/com/bankapplication/bankapplication/Service/CustomerService.java
+++ bankapplication-backend/src/main/java/com/bankapplication/bankapplication/Service/CustomerService.java
@@ -1,25 +1,57 @@
 package com.bankapplication.bankapplication.Service;
 
 import com.bankapplication.bankapplication.JPARepository.CustomerJPA;
+import com.bankapplication.bankapplication.JPARepository.RoleJPA;
 import com.bankapplication.bankapplication.Model.Customer;
+import com.bankapplication.bankapplication.Model.Role;
+import com.bankapplication.bankapplication.Types.RoleType;
 import com.bankapplication.bankapplication.Util.CustomerUtil;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 
+import javax.transaction.Transactional;
 import java.math.BigDecimal;
 import java.sql.Timestamp;
-import java.util.List;
-import java.util.Optional;
+import java.util.*;
 
 @Service
-public class CustomerService {
+@Transactional
+public class CustomerService implements UserDetailsService {
 
     private final CustomerJPA customerJPA;
     private final CustomerUtil customerUtil = new CustomerUtil();
+    private final RoleJPA roleJPA;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public CustomerService(CustomerJPA customerJPA) {
+    public CustomerService(CustomerJPA customerJPA, RoleJPA roleJPA, PasswordEncoder passwordEncoder) {
         this.customerJPA = customerJPA;
+        this.roleJPA = roleJPA;
+        this.passwordEncoder = passwordEncoder;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        Customer customer = customerJPA.findByEmailAddress(email);
+
+        if (customer == null) {
+            throw new UsernameNotFoundException("Customer not found in the database");
+        }
+
+        List<GrantedAuthority> authorities = new ArrayList<>();
+        customer.getRoles().forEach(role -> {
+            GrantedAuthority grantedAuthority = new SimpleGrantedAuthority("ROLE_"+role.getRoleName());
+            authorities.add(grantedAuthority);
+        });
+
+        return new org.springframework.security.core.userdetails.User(customer.getEmailAddress(),
+                customer.getPassword(), authorities);
     }
 
     public List<Customer> getAllCustomer() {
@@ -35,7 +67,25 @@ public class CustomerService {
         Timestamp registrationDate = customerUtil.registrationDate();
         customer.setRegistrationDate(registrationDate);
         customer.setBalance(defaultBalance);
+
+        customer.setPassword(passwordEncoder.encode(customer.getPassword()));
+
         customerJPA.save(customer);
+        addRuleToUser(customer.getId(), RoleType.USER);
+    }
+
+    public void addRuleToUser(Long customerId, RoleType roleName) {
+        Customer customer = customerJPA.findById(customerId).get();
+        Role role = roleJPA.findByRoleName(roleName);
+        customer.getRoles().add(role);
+    }
+
+    public Customer ownInformation(String email) {
+        return customerJPA.findByEmailAddress(email);
+    }
+
+    public BigDecimal ownBalance(String email) {
+        return customerJPA.findByEmailAddress(email).getBalance();
     }
 
     public void updateCustomer(Customer customer) {
@@ -50,6 +100,8 @@ public class CustomerService {
         return customerJPA.findById(customerId).get().getBalance();
     }
 
-
+    public void saveRole(Role role) {
+        roleJPA.save(role);
+    }
 
 }
