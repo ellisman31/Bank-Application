23804a2b80c89d67b92cb06d43a8bc5ff0291543
ellisman31 <akos99@protonmail.com>
2022-02-23 02:50:55 +0100

    feat(#10): get Customer by email address

--- bankapplication-backend/src/main/java/com/bankapplication/bankapplication/JPARepository/CustomerJPA.java
+++ bankapplication-backend/src/main/java/com/bankapplication/bankapplication/JPARepository/CustomerJPA.java
@@ -6,4 +6,7 @@ import org.springframework.stereotype.Repository;
 
 @Repository
 public interface CustomerJPA extends JpaRepository<Customer, Long> {
+
+    Customer findByEmailAddress(String emailAddress);
+
 }
